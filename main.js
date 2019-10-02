32d61dee74ffac696c88a48e452ede803447ac79
ArthurBArre <arthur_barre@outlook.fr>
2019-10-03 00:34:12 +0200

    test and deploy

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-function App() {
-  this.app = document.querySelector("#app");
-  this.valueZ = document.getElementById("valueZ")
-  this.forceValue = "";
-  pressure = require('pressure')
-  pressure.set(app, {
-    change: function (force, event) {
-      console.log(force);
-      this.forceValue = force;
-      this.valueZ.innerHTML = this.forceValue;
-    }
-  });
+const pressure = require('pressure')
+var app = document.querySelector("#app");
+var valueZ = document.getElementById("valueZ")
+var forceValue = "";
 
-}
+pressure.set(app, {
+  change: function (force, event) {
+    console.log(force);
+    forceValue = force;
+    valueZ.innerHTML = forceValue;
+  }
+});
