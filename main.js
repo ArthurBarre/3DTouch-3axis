a9ff16dca3c68dd0c6103bdfa25780bd93768d43
ArthurBArre <arthur_barre@outlook.fr>
2019-10-06 12:18:22 +0200

    change handle

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const pressure = require('pressure')
+// const pressure = require('pressure')
 var app = document.querySelector("#app");
-var valueZ = document.getElementById("valueZ")
+var valueZ = document.getElementById("valueZ");
+var valueX = document.getElementById("valueX");
+var valueY = document.getElementById("valueY");
+
 var forceValue = "";
 
-pressure.set(app, {
-  change: function (force, event) {
-    console.log(force);
-    forceValue = force;
-    valueZ.innerHTML = forceValue;
-  }
-});
+// pressure.set(app, {
+//   change: function (force, event) {
+//     console.log(force);
+//     forceValue = force;
+//     valueZ.innerHTML = forceValue;
+//   }
+// });
+
+app.addEventListener("touchmove", handleClickPosition, false);
+
+function handleClickPosition(e) {
+  let y = e.clientY;
+  console.log('valueY: ', valueY);
+  let x = e.clientX;
+  valueX.innerHTML = "x = " + x;
+  valueY.innerHTML = "y = " + y;
+  console.log('valueX: ', valueX);
+}
