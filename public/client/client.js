691c1a90dbc82cbd550b5b261df0f7d75b236f6c
MacArthur <arthurbarre.js@gmail.com>
2019-11-06 18:48:43 +0100

    add 3d representation

--- public/client/client.js
+++ public/client/client.js
@@ -5,11 +5,20 @@ var x_witness = document.getElementById('x');
 var y_witness = document.getElementById('y');
 var z_witness = document.getElementById('z');
 var socket = io.connect();
+
+// var xV = 0;
+// var yV = 0;
+// var zV = 0;
+
 socket.on('positionUpdate', function (data) {
   console.log('data: ', data);
   let X = data[0];
   let Y = data[1];
   let Z = data[2];
+  // x_array.push(X);
+  // console.log(x_array)
+  // y_array.push(Y);
+  // z_array.push(Z);
   console.log('Z: ', Z);
   x.textContent = "X = " + X;
   x_witness.style.width = X * 300 + 'px';
@@ -20,11 +29,36 @@ socket.on('positionUpdate', function (data) {
 });
 
 var position;
-socket.on('positionUpdate', function (data) {
-  console.log('data: ', data);
-  var X = 1;
-  var Y = 2;
-  var Z = 3;
-  //data[2];
-  position = { X, Y, Z }
-})
+
+// socket.on('positionUpdate', function (data) {
+//   console.log('data: ', data);
+//   var X = 1;
+//   var Y = 2;
+//   var Z = 3;
+//   //data[2];
+//   position = { X, Y, Z }
+// })
+
+
+
+// const graph = document.getElementById('graph');
+// var x_array = [0];
+// var y_array = [0];
+// var z_array = [0];
+// var c = '#E63B24';
+
+// Plotly.plot('graph', [{
+//   type: 'scatter3d',
+//   mode: 'lines',
+//   x: x_array,
+//   y: y_array,
+//   z: z_array,
+//   opacity: 0.7,
+//   line: {
+//     width: 5,
+//     color: c,
+//     colorscale: 'Viridis'
+//   }
+// }], {
+//   height: 640
+// });
